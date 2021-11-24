fbfd1af040124e545d906e51c06801a0565950b4
ashissikder <ashissikder678@gmail.com>
2021-11-24 01:29:49 -0500

    menu to team uploaded

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // banner slider
+    $('.banner-slider').slick({
+        arrows:false,
+        dots:true,
+    });
+    // about vedio pop-up
+    $('.venobox').venobox();
+});
