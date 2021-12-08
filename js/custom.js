55adf8fb0648d8c02362e34a5fe04e2af287664b
ashissikder <ashissikder678@gmail.com>
2021-12-08 00:37:03 -0500

    funfact to footer uploaded

--- js/custom.js
+++ js/custom.js
@@ -15,5 +15,61 @@ $(document).ready(function(){
         slidesToShow:2,
         autoplay:true,
         speed:700,
+        responsive:[
+            {
+                breakpoint:768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots:false,
+                    arrows:false,
+                }
+            },    
+        ],
+    });
+    // funfact counterUp
+    $('.counter').counterUp({
+        delay: 20,
+        time: 1500,
+    });
+    // market slider
+    $('.market-slider').slick({
+        slidesToShow:5,
+        slidesToScroll:1,
+        centerMode:true,
+        centerPadding:'0px',
+        autoplay:true,
+        speed:400,
+        prevArrow:'<i class="fas fa-chevron-left slick-prev prev-arrow"></i>',
+        nextArrow:'<i class="fas fa-chevron-right slick-next next-arrow"></i>',
+        responsive: [
+            {
+                breakpoint:992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots:false,
+                    arrows:false,
+                }
+            },    
+            {
+                breakpoint:768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots:false,
+                    arrows:false,
+                }
+            },    
+            {
+                breakpoint:576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots:false,
+                    arrows:false,
+                }
+            },    
+        ],
     });
 });
